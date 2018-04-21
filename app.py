e15e16d0c7195683f0f386d3be3f1faf486ca81d
Henry Jones <wjones5@twu.edu>
2018-04-21 14:23:59 -0500

    Okay fixing it again

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_ask import Ask, statement
 app = Flask(__name__)
 ask = Ask(app, '/')
 
-@ask.intent('HelloIntent')
-def hello(firstname):
+@ask.launch
+def start_skill():
     welcome_message = "Hi, your app is working"
     return statement(welcome_message)
 
