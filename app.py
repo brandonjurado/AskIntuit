03759283fb1a4449693b780dd6688177475f1039
Henry Jones <wjones5@twu.edu>
2018-04-21 12:39:19 -0500

    Added Simple Voice Interface for testing deployment

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_ask import Ask, statement
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.intent('HelloIntent')
+def hello(firstname):
+    text = render_template('hello', firstname=firstname)
+    return statement(text).simple_card('Hello', text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
