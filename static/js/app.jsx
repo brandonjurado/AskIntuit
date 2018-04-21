de0fee6ac04ff77eb1f21094a1c82ba504f6b31c
Michael Osei <mosei27@live.com>
2018-04-21 14:59:37 -0500

    Add complete frontend setup

---
+++ static/js/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './stylesheets/main.scss';
+import ChatBot from 'react-simple-chatbot';
+
+class App extends  React.Component {
+    render() {
+        return( 
+            <div className="app-view">
+                
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('react-app'));
