2a11792c977973ab30be925a11e5984024850463
Brandon Jurado <brandonjurado13@yahoo.com>
2018-04-21 16:49:55 -0500

    Meaningful variables

--- earthxhackData.py
+++ earthxhackData.py
@@ -4,25 +4,24 @@ import re
 from collections import OrderedDict
 
 search_term_string="how to file taxes on a roth ira"
-r = requests.get("https://accountants-community.intuit.com/search?utf8=%E2%9C%93&q="+search_term_string)
-data = r.text
-soup = BeautifulSoup(data, 'html.parser')
+page = requests.get("https://accountants-community.intuit.com/search?utf8=%E2%9C%93&q="+search_term_string)
+soup = BeautifulSoup(page.content, 'html.parser')
 
 links = str(soup.find("div", {"id": "search-results"}))
-article_indexs = re.findall('"([^"]*)"', links)
+article_indexes = re.findall('"([^"]*)"', links)
 li = []
-for i in article_indexs:
+for i in article_indexes:
   if i.startswith("Article"):
     li.append(i)
-final_indexs = []
+final_indexes = []
 for i in li:
   x = i.split()
-  final_indexs.append(x[1])
-final_indexs = list(OrderedDict.fromkeys(final_indexs))
+  final_indexes.append(x[1])
+final_indexes = list(OrderedDict.fromkeys(final_indexes))
 answer_array = []
 question_array = []
 #https://accountants-community.intuit.com/articles/1609578
-for i in final_indexs:
+for i in final_indexes:
   rr = requests.get("https://accountants-community.intuit.com/articles/" + i)
   result_data = rr.text
   soup = BeautifulSoup(result_data, 'html.parser')
@@ -34,10 +33,11 @@ for i in final_indexs:
       end2 = 'Was this article helpful?'
       s = row.text
       questions = s[s.find(start1) + len(start1):s.rfind(end1)]
-      questions = questions.replace('\n', '')
+      print(questions)
+      questions = questions.replace('\n', ' ')
       answers = s[s.find(start2) + len(start2):s.rfind(end2)]
       answers = answers.replace('\n', '')
       question_array.append(questions)
       answer_array.append(answers)
-print(question_array)
+print(question_array[0])
 print(answer_array)
