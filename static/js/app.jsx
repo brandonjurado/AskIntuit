47d61c2c5dd9d567ea578d0ac7ede425f98f2303
Michael Osei <mosei27@live.com>
2018-04-21 15:30:57 -0500

    Update frontend app

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './stylesheets/main.scss';
+import './main.scss';
 import ChatBot from 'react-simple-chatbot';
 
 class App extends  React.Component {
+    constructor(){
+        super();
+        this.initialSteps = [
+              {
+                id: '0',
+                message: 'Hi, you can ask me your sexy tax questions!',
+                trigger: '1',
+              }
+        ];
+    }
+
     render() {
         return( 
             <div className="app-view">
-                
+                <ChatBot steps={this.initialSteps} />
             </div>
         );
     }
